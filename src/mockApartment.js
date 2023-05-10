54c8e03614d540ac784f7f7969c42ce86f1927c4
Jose Luis Salas Jr <125709745+salas89jl@users.noreply.github.com>
2023-05-10 09:45:34 -0700

    added mock data for user and apartments

---
+++ src/mockApartment.js
@@ -0,0 +1,46 @@
+let mockApartment = [
+    {
+        id: 1, 
+        street: ' 123 key st',
+        unit: 'd',
+        city: 'San Diego',
+        state: 'NV',
+        square_footage: 1100,
+        price: '2222',
+        bedrooms: 3,
+        bathroom: 3.93,
+        pets: 'yes',
+        image: "https://cdn.pixabay.com/photo/2015/10/26/22/48/house-1008053__480.jpg",
+        user_id: 1
+    },
+    {
+        id: 2, 
+        street: ' 456 lock dr',
+        unit: 'b.3',
+        city: 'Uptown',
+        state: 'CT',
+        square_footage: 2000,
+        price: '4000',
+        bedrooms: 2,
+        bathroom: 6,
+        pets: 'no',
+        image: "https://cdn.pixabay.com/photo/2016/12/06/23/53/norcross-ga-1888299__480.jpg",
+        user_id: 1
+    },
+    {
+        id: 3, 
+        street: ' 897 lost key ln',
+        unit: '3',
+        city: 'Las Vegas',
+        state: 'NV',
+        square_footage: 2093,
+        price: '2100',
+        bedrooms: 3,
+        bathroom: 3,
+        pets: 'yes',
+        image: "https://cdn.pixabay.com/photo/2020/05/26/07/26/hong-kong-5221804__480.jpg",
+        user_id: 2
+    }
+]
+
+export default mockApartment
